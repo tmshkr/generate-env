2157d23e94545a85938a3c56b9755e737838f24e
Tim Shaker <tim@tmshkr.com>
2023-11-28 17:57:27 -0800

    maskSecrets

--- main.js
+++ main.js
@@ -1,10 +1,11 @@
 const fs = require("fs");
 const path = require("path");
 
-const { INPUT_ENV, INPUT_FILENAME, GITHUB_WORKSPACE } = process.env;
+const { INPUT_ENV, INPUT_FILENAME, INPUT_SECRET_KEYS, GITHUB_WORKSPACE } =
+  process.env;
 
 try {
-  const filepath = path.join(GITHUB_WORKSPACE, INPUT_FILENAME);
+  var filepath = path.join(GITHUB_WORKSPACE, INPUT_FILENAME);
   if (fs.existsSync(filepath)) {
     console.log(`${INPUT_FILENAME} already exists. Appending...`);
     fs.appendFileSync(filepath, "\n" + INPUT_ENV);
@@ -14,7 +15,20 @@ try {
     fs.writeFileSync(filepath, INPUT_ENV);
     console.log(`Created ${filepath}`);
   }
+
+  maskSecrets(filepath);
 } catch (error) {
   console.error(`There was an error creating ${filepath}`);
   throw error;
+}
+
+function maskSecrets(filepath) {
+  const secrets = new Set(INPUT_SECRET_KEYS.split(","));
+  const file = fs.readFileSync(filepath, "utf-8");
+  file.split("\n").forEach((line) => {
+    const [key, value] = line.split("=");
+    if (secrets.has(key)) {
+      console.log(`::add-mask::${value}`);
+    }
+  });
 }
